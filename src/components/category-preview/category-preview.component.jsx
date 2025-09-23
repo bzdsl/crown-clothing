e020893b93363bb5b544f78b6eee13da0eb97c23
Thang Duc Nguyen <75111229+bzdsl@users.noreply.github.com>
2025-09-23 15:20:06 +0700

    converted scss to styled-component

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,25 +1,30 @@
 /** @format */
 
-import "./category-preview.styles.scss";
+import {
+  CategoryPreviewContainer,
+  Title,
+  Preview,
+} from "./category-preview.styles";
 import ProductCard from "../product-card/product-card.component";
 import { Link } from "react-router-dom";
 
 const CategoryPreview = ({ title, products }) => {
   return (
-    <div className="category-preview-container">
-      <h2>
+    <CategoryPreviewContainer>
+      <Title>
         <Link className="title" to={title}>
           {title.toUpperCase()}
         </Link>
-      </h2>
-      <div className="preview">
+      </Title>
+      <Preview>
+        ;
         {products
           .filter((_, idx) => idx < 4)
           .map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
-      </div>
-    </div>
+      </Preview>
+    </CategoryPreviewContainer>
   );
 };
 
